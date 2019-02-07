ab97a3d770e211c077a133b968be7ed13bd811fe
Acoustic6 <6dorog@mail.ru>
2019-02-07 22:35:53 +0300

    function declaration

--- CppCherno/CppCherno/main.cpp
+++ CppCherno/CppCherno/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+// function declaration. compiler believes Log exists until linker comes in.
+// if such function will not be found than linker error will appear.
+// Ctrl + F7 - with no linker. Ctrl + F5 - with linker.
+
+void Log(const char* message); 
+
 int main()
 {
+	Log("Hello World!");
+	std::cin.get();
 }
