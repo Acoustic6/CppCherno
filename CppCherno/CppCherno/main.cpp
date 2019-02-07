8919ca72dfef6bfbff4d6c63ed2a1509e4c2d2b8
Acoustic6 <6dorog@mail.ru>
2019-02-07 22:37:34 +0300

    introducing header file

--- CppCherno/CppCherno/main.cpp
+++ CppCherno/CppCherno/main.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-
-// function declaration. compiler believes Log exists until linker comes in.
-// if such function will not be found than linker error will appear.
-// Ctrl + F7 - with no linker. Ctrl + F5 - with linker.
-
-void Log(const char* message); 
+#include "log.h"
 
 int main()
 {
