3d4040b154849c7ad8d972ed0d1753fcbb30f507
Acoustic6 <6dorog@mail.ru>
2019-02-07 22:44:53 +0300

    simple pointer

--- CppCherno/CppCherno/main.cpp
+++ CppCherno/CppCherno/main.cpp
@@ -3,6 +3,10 @@
 
 int main()
 {
-	Log("Hello World!");
+	char num = 6;
+	char* ptr = &num;
+	//set pointer to the memory adress of num by &num.
+	//use *ptr to retrieve the value kept in it's memory adress(pointer to it).
+
 	std::cin.get();
 }
