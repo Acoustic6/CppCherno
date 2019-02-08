397b2a5c736d337bba5b0eb74359ccb04c2546d2
Acoustic6 <6dorog@mail.ru>
2019-02-08 21:39:58 +0300

    passing variable by reference

--- CppCherno/CppCherno/main.cpp
+++ CppCherno/CppCherno/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 
+void Increment(int a)
+{
+	//a's adress is now 0x001CF74C. new variable in new adress.
+	a++;
+}
+
+void IncrementByPointer(int* b)
+{
+	(*b)++;// to increment b we passed here we may pass b's memory adress.
+}
+
+void IncrementUsingRef(int& c)
+{
+	c++;//nost propper way to increment is passing argument by ref.
+}
+
 int main()
 {
-	int a = 5;
-	int* b = &a;
-	int& ref = a;
-	ref = 2; // a now == 2
+	int a = 5;// adress: 0x001CF82C
+	Increment(a);//a will no be incremented here
+
+	int b = 5;
+	IncrementByPointer(&b);
+
+	int c = 5;
+	IncrementUsingRef(c);
 }
