98f165a343187b682dd646aa6288c7e16f226149
Acoustic6 <6dorog@mail.ru>
2019-02-08 21:21:56 +0300

    simple reference

--- CppCherno/CppCherno/main.cpp
+++ CppCherno/CppCherno/main.cpp
@@ -2,9 +2,8 @@
 
 int main()
 {
-	//please, allocate for me 8 bytes of memory:
-	char* buffer = new char[8]; // points to the first byte of an array
-	memset(buffer, 6, 8);//sets every of 8 bytes to 6, beginning from byte which pointer buffer points to.
-
-	delete buffer;
+	int a = 5;
+	int* b = &a;
+	int& ref = a;
+	ref = 2; // a now == 2
 }
